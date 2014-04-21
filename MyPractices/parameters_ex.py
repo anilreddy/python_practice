7be68efc5b747cede5b1fe85f55d489df930537a
Anil Kumar Reddy Gaddam <ganilreddy@hotmail.com>
2014-04-21 08:10:24 +0530

    Create parameters_ex.py

---
+++ MyPractices/parameters_ex.py
@@ -0,0 +1,10 @@
+# Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The Script is called:", script
+print "Your first variable:", first
+print "Your second variable:", second
+print "Your third variable:", third
